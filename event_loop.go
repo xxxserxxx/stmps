488edd07aa98a167ef87425839ae80102aa14b28
spezifisch <spezifisch23@proton.me>
2023-10-26 11:00:25 +0200

    fix missing draw call in log

--- event_loop.go
+++ event_loop.go
@@ -38,7 +38,7 @@ func (ui *Ui) guiEventLoop() {
 		select {
 		case msg := <-ui.logger.Prints:
 			// handle log page output
-			ui.app.QueueUpdate(func() {
+			ui.app.QueueUpdateDraw(func() {
 				line := time.Now().Local().Format("(15:04:05) ") + msg
 				ui.logList.InsertItem(0, line, "", 0, nil)
 
