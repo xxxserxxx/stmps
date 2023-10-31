7da9a4c6ba2db367e691bef582a13ccd9ebdde5a
spezifisch <spezifisch23@proton.me>
2023-10-31 11:19:22 +0100

    fix license headers

--- logger/logger.go
+++ logger/logger.go
@@ -1,5 +1,5 @@
 // Copyright 2023 The STMP Authors
-// SPDX-License-Identifier: GPL-3.0-or-later
+// SPDX-License-Identifier: GPL-3.0-only
 
 package logger
 
--- remote/interfaces.go
+++ remote/interfaces.go
@@ -1,3 +1,6 @@
+// Copyright 2023 The STMP Authors
+// SPDX-License-Identifier: GPL-3.0-only
+
 package remote
 
 type ControlledPlayer interface {
