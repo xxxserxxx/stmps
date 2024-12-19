574b6605e2349c025021f3628c2ddb08e2178109
Sean E. Russell <ser@ser1.net>
2024-12-18 18:06:35 -0600

    change: rolls back the (unrelated, unnecessary) logger changes
    Signed-off-by: Sean E. Russell <ser@ser1.net>

--- logger/logger.go
+++ logger/logger.go
@@ -3,53 +3,24 @@
 
 package logger
 
-import (
-	"fmt"
-	"io"
-	"os"
-)
+import "fmt"
 
 type Logger struct {
 	Prints chan string
-	fout   io.WriteCloser
 }
 
-func Init(file string) *Logger {
-	l := Logger{
-		Prints: make(chan string, 100),
-	}
-	if file != "" {
-		var err error
-		l.fout, err = os.Create(file)
-		if err != nil {
-			fmt.Printf("error opening requested log file %q\n", file)
-		}
-	}
-	return &l
+func Init() *Logger {
+	return &Logger{make(chan string, 100)}
 }
 
 func (l *Logger) Print(s string) {
-	if l.fout != nil {
-		fmt.Fprintf(l.fout, "%s\n", s)
-	}
 	l.Prints <- s
 }
 
 func (l *Logger) Printf(s string, as ...interface{}) {
-	if l.fout != nil {
-		fmt.Fprintf(l.fout, s, as...)
-		fmt.Fprintf(l.fout, "\n")
-	}
 	l.Prints <- fmt.Sprintf(s, as...)
 }
 
 func (l *Logger) PrintError(source string, err error) {
 	l.Printf("Error(%s) -> %s", source, err.Error())
-}
-
-func (l *Logger) Close() error {
-	if l.fout != nil {
-		return l.fout.Close()
-	}
-	return nil
 }
